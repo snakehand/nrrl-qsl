831f203977c4bbbea29685f3185c821f88898c5e
Frank Andrew Stevenson <frank.stevenson@magseis.com>
2020-04-04 13:40:38 +0200

    Initial commit. Proof of concept. TODO:   GUI (?)   Config file for group number translations   Specify output file (currently stdout)

---
+++ src/main.rs
@@ -0,0 +1,93 @@
+extern crate serde;
+
+mod tripletex;
+
+use std::collections::HashSet;
+use std::env;
+use std::ffi::OsString;
+
+// Translate group number code from customer file to QSL sorting code (if valid)
+fn translate_group(group: u32) -> Option<u32> {
+    if group == 0 {
+        return None; // SK
+    } else if group < 100 {
+        return Some(group + 100);
+    }
+    let g = match group {
+        175 => 19,    // Haldengruppen av NRRL
+        240 => 9999,  // Østerdalsgruppen av NRRL
+        318 => 24,    // Hortengruppen av NRRL
+        325 => 27,    // Larvikgruppen av NRRL
+        354 => 53,    // Nedre Hallingdalsgruppen av NRRL
+        357 => 12,    // Hallingdalsgruppen av NRRL
+        367 => 34,    // Notoddengruppen av NRRL
+        375 => 29,    // Drangedalgruppen av NRRL
+        400 => 9999,  // Stavangergruppen av NRRL
+        413 => 42,    // Ryfylkegruppen av NRRL
+        420 => 132,   // Saudagruppen
+        437 => 47,    // Egersundsgruppen av NRRL
+        445 => 11,    // Listagruppen av NRRL
+        554 => 41,    // Haugalandgruppen av NRRL
+        848 => 91,    // Andøygruppen av NRRL
+        865 => 87,    // Mosjøengruppen av NRRL
+        917 => 117,   // Svalbardgruppen av NRRL
+        976 => 89,    // Nordkappgruppen av NRRL
+        995 => 97,    // Vardøgruppen av NRRL
+        9999 => 9999, // Ingen gruppetilknytning
+        g => g / 10,  // Default rule
+    };
+    if g >= 1000 {
+        None
+    } else {
+        Some(g)
+    }
+}
+
+fn main() {
+    let args: Vec<OsString> = env::args_os().collect();
+    if args.len() < 2 {
+        println!("usage: {} members.csv", args[0].to_string_lossy());
+        return;
+    }
+    let ml = match tripletex::read_members(&args[1]) {
+        Ok(d) => d,
+        Err(e) => {
+            println!("Failed: {:?}", e);
+            return;
+        }
+    };
+    let mut groups = HashSet::new();
+    for m in &ml {
+        // println!("{:?}", m);
+        if m.customer_number.len() == 0 {
+            continue; // Not a member
+        }
+        let callsign_start = match m.name.find("(") {
+            Some(p) => p + 1,
+            None => continue,
+        };
+        let callsign_end = match m.name.find(")") {
+            Some(p) => p,
+            None => continue,
+        };
+        if callsign_start > callsign_end {
+            continue;
+        }
+        let callsign = &m.name[callsign_start..callsign_end];
+        // println!("Callsign: {}", callsign);
+        let group = match m.category_number2.parse::<u32>() {
+            Ok(g) => g,
+            Err(_) => continue,
+        };
+        groups.insert(group);
+        match translate_group(group) {
+            Some(g) => {
+                print!("{},{:03}\r\n", callsign, g);
+            }
+            None => (),
+        }
+    }
+    let mut groups: Vec<u32> = groups.iter().map(|v| *v).collect();
+    groups.sort();
+    // println!("{:?}", groups);
+}
---
+++ src/tripletex.rs
@@ -0,0 +1,70 @@
+use csv::StringRecord;
+use serde::Deserialize;
+use std::error::Error;
+use std::ffi::OsStr;
+use std::fs::File;
+
+#[derive(Debug, Deserialize)]
+pub struct Member {
+    pub customer_number: String,
+    pub supplier_number: String,
+    pub name: String,
+    pub organisation_number: String,
+    pub telephone_number: String,
+    pub mobile_number: String,
+    pub fax_number: String,
+    pub email: String,
+    pub postal_adress1: String,
+    pub postal_adress2: String,
+    pub postal_code: String,
+    pub city: String,
+    pub country: String,
+    pub business_adress1: String,
+    pub business_adress2: String,
+    pub business_postal_code: String,
+    pub business_city: String,
+    pub business_country: String,
+    pub category_number1: String,
+    pub category_name1: String,
+    pub category_number2: String,
+    pub category_name2: String,
+    pub category_number3: String,
+    pub category_name3: String,
+}
+
+pub fn read_members(filename: &OsStr) -> Result<Vec<Member>, Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    rdr.set_headers(StringRecord::from(vec![
+        "customer_number",
+        "supplier_number",
+        "name",
+        "organisation_number",
+        "telephone_number",
+        "mobile_number",
+        "fax_number",
+        "email",
+        "postal_adress1",
+        "postal_adress2",
+        "postal_code",
+        "city",
+        "country",
+        "business_adress1",
+        "business_adress2",
+        "business_postal_code",
+        "business_city",
+        "business_country",
+        "category_number1",
+        "category_name1",
+        "category_number2",
+        "category_name2",
+        "category_number3",
+        "category_name3",
+    ]));
+    let mut contacts = Vec::new();
+    for result in rdr.deserialize() {
+        let record: Member = result?;
+        contacts.push(record)
+    }
+    Ok(contacts)
+}
